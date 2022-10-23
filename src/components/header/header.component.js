5b273b18a02bbd6e72325c040973f6c16836627a
egor123 <egor.lukyanenko@gmail.com>
2022-10-23 15:12:19 +0300

    main layout

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,3 +1,13 @@
-export default function(element){
-   //TODO AUTO GENERATED CODE
+const changeStateDelta = 200;
+
+export default function (element) {
+   var previousScrollY = window.scrollY;
+   var offset = 0;
+
+   onscroll = () => {
+      var delta = previousScrollY - window.scrollY;
+      previousScrollY = window.scrollY;
+      offset = (delta * offset) >= 0 ? offset + delta : 0;
+      element.classList.toggle("hidden", offset < -changeStateDelta);
+   };
 }
