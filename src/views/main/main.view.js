7ea0767f1651a1cee1c6f8c2ee69eb8d2788ad66
egor123 <egor.lukyanenko@gmail.com>
2022-11-06 15:30:28 +0200

    main page retro effects

--- src/views/main/main.view.js
+++ src/views/main/main.view.js
@@ -1,7 +1,14 @@
-export function load(element){
-    
+var moon;
+
+export function load(element) {
+    moon = document.getElementById("moon");
+    parallax();
+    document.addEventListener("scroll", parallax);
 }
-export function unload(element){
-    
-    
+export function unload(element) {
+    document.removeEventListener("scroll", parallax);
+}
+
+function parallax() {
+    moon.style.transform = `translateY(${-100 + window.scrollY / 2.5}px)`;
 }
