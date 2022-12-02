93fe662be953dcf68323c9fe79f2d4b149685a4d
egor123 <egor.lukyanenko@gmail.com>
2022-12-02 12:35:38 +0200

    meaning of life

---
+++ src/views/question/question.view.js
@@ -0,0 +1,12 @@
+/**
+* @param {HTMLElement} element 
+**/
+export function load(element) {
+    //TODO AUTO GENERATED CODE
+}
+/**
+* @param {HTMLElement} element 
+**/
+export function unload(element) {
+    //TODO AUTO GENERATED CODE
+}
