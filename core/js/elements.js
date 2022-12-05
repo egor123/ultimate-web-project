bb65c40d02295e227ef84269d3eea31f7b744ad6
egor123 <egor.lukyanenko@gmail.com>
2022-12-05 21:43:56 +0200

    html path fix

--- core/js/elements.js
+++ core/js/elements.js
@@ -20,7 +20,7 @@ export class ElementsUtils {
         });
     }
     static async getHtml(path) {
-        return await fetch(path + ".html")
+        return await fetch("../../" + path + ".html")
             .then(t => t.text())
             .then(t => t.replaceAll(/<.*template>/g, ''));
     }
